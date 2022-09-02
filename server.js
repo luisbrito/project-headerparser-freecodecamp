3c78aaef77f817c1b8c2fe1a36b198b24b38e16d
Luis Brito <luisfernandobrito@me.com>
2022-09-02 16:21:39 -0300

    update server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+app.get("/api/whoami", (req, res) => {
+// req contain a property called header where all the details of header are stored 
+  let address = req.headers.host; 
+  let language = req.headers["accept-language"];
+  let software = req.headers["user-agent"];
+  res.json({ ipaddress: address, language: language, software: software });
+});
+
+// listen for requests :)
+var listener = app.listen(3000, function () {
+  console.log("Your app is listening on port " + 3000);
+});
